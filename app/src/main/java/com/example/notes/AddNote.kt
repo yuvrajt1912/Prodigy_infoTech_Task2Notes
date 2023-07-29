876e6e7fe3648296cfbb2176f8c2fe116deed798
Yuvraj Tyagi <yuvraaj.tyagi19@gmail.com>
2023-07-29 15:30:36 +0530

    NOTES

---
+++ app/src/main/java/com/example/notes/AddNote.kt
@@ -0,0 +1,81 @@
+package com.example.notes
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.notes.databinding.ActivityAddnoteBinding
+import com.example.notes.models.Note
+import java.text.SimpleDateFormat
+import java.util.Date
+
+class AddNote : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddnoteBinding
+
+    private lateinit var note: Note
+    private lateinit var old_note : Note
+    var isUpdate = false
+
+
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityAddnoteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        try{
+
+            old_note = intent.getSerializableExtra("current_note") as Note
+            binding.etTitle.setText(old_note.title)
+            binding.etNote.setText(old_note.note)
+            isUpdate = true
+        }catch(e : Exception){
+
+            e.printStackTrace()
+        }
+
+        binding.imgCheck.setOnClickListener {
+
+            val title = binding.etTitle.text.toString()
+            val note_desc = binding.etNote.text.toString()
+
+            if(title.isNotEmpty() || note_desc.isNotEmpty()){
+
+                val formatter = SimpleDateFormat("EEE, d MMM yyy HH:mn a")
+
+                if(isUpdate){
+
+                    note = Note(
+                        old_note.id,title,note_desc,formatter.format(Date())
+                    )
+                }else {
+
+                    note = Note(
+                        null,title,note_desc,formatter.format(Date())
+                    )
+                }
+
+                val intent = Intent()
+                intent.putExtra("note",note)
+                setResult(Activity.RESULT_OK,intent)
+                finish()
+
+            }else {
+
+                Toast.makeText(this@AddNote,"Please enter some data",Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+        }
+
+        binding.imgBackArrow.setOnClickListener {
+
+            onBackPressed()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/notes/utilities/constants.kt
@@ -0,0 +1,3 @@
+package com.example.notes.utilities
+
+const val DATABASE_NAME = "note_database"
